0bd1774672afeb9b83ec5c81a7801e6cc795f572
Sai Vineet <saivineet89@gmail.com>
2015-12-28 12:21:49 +0530

    PEP8

--- config.example.py
+++ config.example.py
@@ -15,14 +15,15 @@ RELOCK_EVERYTIME = True
 # WARNING: REPO NAME (I.E KEY) MUST == REPO NAME ON GITHUB. ONLY THEN
 # WILL git-svn-sync KNOW WHICH REPO TO MESS WITH.
 DIRECTORY_MAP = {
-    'apertium-html-tools': ('apertium-html-tools',
+    'apertium-html-tools': (
+        'apertium-html-tools',
         'https://github.com/goavki/apertium-html-tools.git',
         'https://svn.code.sf.net/p/apertium/svn/trunk/apertium-tools/apertium-html-tools/'),
 }
 # Default settings are for apertium-html-tools
 
 
-# Commit name on github, new sha[:6], Committer name and Committer email 
+# Commit name on github, new sha[:6], Committer name and Committer email
 COMMIT_MESSAGE = "git-svn-sync: {} ({}) - {}<{}>"
 INITIAL_COMMIT_MESSAGE = 'git-svn-sync: Sync repository'
 # Repo name, sha-old, sha-new
